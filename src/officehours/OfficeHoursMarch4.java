3f59c38729bf69ec8c3fcf35c521e498c5375e8c
CybertekSchool <github@cybertekschool.com>
2019-03-05 17:42:59 -0500

    initial commit

---
+++ src/officehours/OfficeHoursMarch4.java
@@ -0,0 +1,15 @@
+package officehours;
+
+public class OfficeHoursMarch4 {
+	public static void main(String[] args) {
+		int i = 13 , j = 5 ;
+		int k = 5; 
+		 
+		 
+		 System.out.println("Multiply result is " + i*j + "." 
+						  	+ " Division result is " + i/j + "." 
+						  	+ " Modular is " + i%j + ".");
+		 System.out.println("Result: "+ (i-k+i)+".");
+	
+	}
+}
