dec274c6ba2a49b36b587b33ff3c66c5ca74f5a8
CybertekSchool <github@cybertekschool.com>
2019-03-13 20:55:07 -0400

    second hour

--- src/day10/GraderApp.java
+++ src/day10/GraderApp.java
@@ -11,17 +11,35 @@ public class GraderApp {
 
 		int score = scan.nextInt();
 
-//		if (score > 90) {
+		if(score > 100 || score < 0){
+			System.out.println("INVALID NUMBER");
+		} else if (score > 90) {
+			System.out.println("Great score : A!");
+		} else if (score > 80) {
+			System.out.println(" Good score : B !!");
+		} else if (score > 70) {
+			System.out.println(" OK SCORE : C !!");
+		} else {
+			System.out.println("STDUY HARDER!!!!!");
+		}
+		
+//		if (score > 90){
 //			System.out.println("Great score : A!");
-//		} else if (score > 80) {
+//		}
+//		if (score > 80 && score <= 90) {
 //			System.out.println(" Good score : B !!");
-//		} else if (score > 70) {
-//			System.out.println(" OK SCORE : C !!");
-//		} else {
-//			System.out.println("STDUY HARDER!!!!!");
+//		}
+//		if (score > 70 && score <= 80) {
+//			System.out.println(" Good score : C !!");
+//		}
+//		
+//		if (score < 70) {
+//			System.out.println(" study Harder !!");
 //		}
 		
 		
+			
+
 		
 		
 		
