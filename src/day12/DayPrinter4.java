134121089885a676b33c74ffcd13dbb0ea5b092a
CybertekSchool <github@cybertekschool.com>
2019-03-17 17:13:43 -0400

    Sunday final  commit

---
+++ src/day12/DayPrinter4.java
@@ -0,0 +1,23 @@
+package day12;
+
+import java.util.Scanner;
+
+public class DayPrinter4 {
+
+	public static void main(String[] args) {
+		
+		String dayWord ; 
+		dayWord = "Monday";
+		dayWord = "Tuesday";
+		dayWord = "Wendesday";
+		dayWord = "Thursday";
+		dayWord = "Friday";
+		dayWord = "Saturday";
+		dayWord = "Sunday";
+
+
+		System.out.println("the day is:" + dayWord);
+
+	}
+
+}
--- src/day12/MoreStringMethod.java
+++ src/day12/MoreStringMethod.java
@@ -13,11 +13,21 @@ public class MoreStringMethod {
 		 * str1.toUpperCase()  ---> return original string with uppercase
 		 * str1.toLowerCase()  ---> return original string with lowercase
 		 * 
+		 * str1.charAt(position of your character as int ) 
+		 * 
+		 * 
 		 * PLEASE COME BACK AT 2:15 PM EST 
 		 * 
 		 * */
 		System.out.println(  weather.toUpperCase()  );
 		System.out.println(  weather.toLowerCase()  );
+		
+		String name = "Sevda" ;  // S-0 e-1 v-2 d-3 a-4
+		
+		char firstChar = name.charAt(0) ; 
+		System.out.println("first character is " + firstChar);
+		
+		
 		
 		
 		
