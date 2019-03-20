7e77a83a550146fcca3a59f7bfddb67596c00ea9
CybertekSchool <github@cybertekschool.com>
2019-03-19 22:24:29 -0400

    Monday String class methods

--- src/officehours/March13.java
+++ src/officehours/March13.java
@@ -9,9 +9,13 @@ public class March13 {
 		System.out.println("Enter price in cents:");
 		itemPrice = scan.nextInt();
 		if (itemPrice >= 25 && itemPrice <= 100 && itemPrice % 5 == 0) {
+			// we are getting amount of change
 			change = 100 - itemPrice;
+			// 55 / 25 = 2, 70/ 25 = 2, 30/ 25 = 1 
 			quarters = change / 25;
+			// 70 / 25 = 2, remainder = 20 / 10 = 2 
 			dimes = change % 25 / 10;
+			// 70 / 25 , remainder = 20/ 10, remainder = 0
 			nickels = change % 25 % 10 / 5;
 			System.out.println(
 					"Your change is " + quarters + " quarters, " + dimes + " dimes, and " + nickels + " nickels.");
