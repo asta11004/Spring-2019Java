1ccb3570b35dac06cffe5fbbfa1d1a977fb6285f
cybertekstudio <cybertekstudio@wsip-70-166-39-41.dc.dc.cox.net>
2019-03-23 17:33:14 -0400

    added days 14 and 15

---
+++ src/day15/Swapping.java
@@ -0,0 +1,24 @@
+package day15;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		
+		int x=10 , y = 20 ; 
+		
+		int temp ; 
+		
+		temp = x ;  // 10 
+		x = y ;  // 20 
+		y = temp ; // 10 
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
