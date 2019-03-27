6793a5b2363f1bcfdcf8ad72f2f4976dab97e872
Cybertek Studio <cybertekstudio@wsip-70-166-39-41.dc.dc.cox.net>
2019-03-26 22:43:14 -0400

    tuesday office hour and classes

---
+++ src/day17/WhileLoop2.java
@@ -0,0 +1,27 @@
+package day17;
+
+public class WhileLoop2 {
+
+	public static void main(String[] args) {
+		
+
+		int x = 0 ; 
+		
+//		while (x<10) {
+//			
+//			System.out.println( "value of x is : " +  ++x  );
+//			
+//		}
+		
+		while (x++ <10) {
+		
+			System.out.println( "value of x is : " +  x  );
+		
+		}
+
+		
+		
+		
+	}
+
+}
