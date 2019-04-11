701a62f6a8664626570206afd7271e15d43daaee
Cybertek Studio <cybertekstudio@wsip-70-166-39-41.dc.dc.cox.net>
2019-04-10 21:59:53 -0400

    codes to char Array , reverse and split

--- src/day26/SplittingTheStrings.java
+++ src/day26/SplittingTheStrings.java
@@ -7,11 +7,21 @@ public class SplittingTheStrings {
 	public static void main(String[] args) {
 		
 		
-		String str = "Java is fun";
+		String str = "Java is fun, java is challenging";
 		
 		String[] words =  str.split(" "); 
-		
 		System.out.println( Arrays.toString(words) );
+		
+		String[] parts =  str.split("a"); 
+		System.out.println( Arrays.toString(parts) );
+		
+		String[] parts2 =  str.split("is"); 
+		System.out.println( Arrays.toString(parts2) );
+		
+		
+		
+		
+		
 		
 
 	}
