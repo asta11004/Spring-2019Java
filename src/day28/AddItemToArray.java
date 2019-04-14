c3684caf3bf1f41d674e120a94fdbbafe79a5432
Cybertek Studio <cybertekstudio@wsip-70-166-39-41.dc.dc.cox.net>
2019-04-14 17:19:15 -0400

    Sunday code

---
+++ src/day28/AddItemToArray.java
@@ -0,0 +1,29 @@
+package day28;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AddItemToArray {
+
+	public static void main(String[] args) {
+		
+		 // create a program to add items to an array by users input 
+		 // {1,23,4,5,6}  , 17  ---> {1,23,4,5,6, 17}  
+		 
+		String[] words = {"coding", "start", "to", "Day", "Perfect", "Sunday"}; 
+		
+		Scanner scan = new Scanner(System.in); 
+		 System.out.println("Enter your word to be added ");
+		 String usersWord = scan.next() ; 
+		 
+		 String[] newWords = Arrays.copyOf(words, words.length + 1) ; 
+		 System.out.println( Arrays.toString(newWords) );
+
+		 int lastSpotIndex = newWords.length-1 ; 
+		 newWords[lastSpotIndex] = usersWord ; 
+		 System.out.println( Arrays.toString(newWords) );
+			 
+
+	}
+
+}
---
+++ src/day28/LastMinutePrint.java
@@ -0,0 +1,20 @@
+package day28;
+
+import java.util.Arrays;
+
+public class LastMinutePrint {
+
+	public static void main(String[] args) {
+		
+		 String[][] groups = {   
+				 				{"BMW","AUDI"},
+				 				{"TOYOTA","HONDA","HYUNDAI"},
+				 				{"SPARTAN","HORSE"}			 		
+		 					} ; 
+
+		 System.out.println( Arrays.deepToString( groups) );
+		 
+		 
+	}
+
+}
