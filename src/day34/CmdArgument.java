c99dc7bf5495058d180cb2879ab4ca72172a3d6a
Cybertek Studio <cybertekstudio@wsip-70-166-39-41.dc.dc.cox.net>
2019-04-27 17:12:33 -0400

    saturday code

--- src/day34/CmdArgument.java
+++ src/day34/CmdArgument.java
@@ -2,8 +2,16 @@ package day34;
 
 public class CmdArgument {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	//String[] args is called command line arguments 
+	// and we can pass value using command line 
+	
+	public static void main(String... args) {
+
+		System.out.println(args.length);
+		for(String item: args){
+			System.out.println(item + " "); 
+		}
 
 	}
 
