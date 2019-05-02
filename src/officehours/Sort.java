cb6ccd18446691ff233aaccc86b38455c08382e9
Cybertek Studio <cybertekstudio@wsip-70-166-39-41.dc.dc.cox.net>
2019-05-01 22:13:56 -0400

    Wrapper review | ArrayList methods

--- src/officehours/Sort.java
+++ src/officehours/Sort.java
@@ -4,18 +4,24 @@ import java.util.Arrays;
 
 public class Sort {
 	public static void main(String[] args) {
-		int[] nums = { 35, 543, 5, 54,65, 87, 65 };
+		int[] nums = { 35, 543, 5, 54, 65, 87, 65 };
 		System.out.println(Arrays.toString(sortArray(nums)));
 	}
 
 	public static int[] sortArray(int[] arr) {
-		for (int i = 0; i < arr.length - 1; i++) {
-			if (arr[i] > arr[i + 1]) {
+		// loop through the array
+		int i=0;
+		while(i < arr.length-1) {
+			// if next value is less than previous
+			// swap them
+			if (arr[i] > arr[i+1]) {
 				int temp = arr[i];
-				arr[i] = arr[i + 1];
-				arr[i + 1] = temp;
+				arr[i] = arr[i+1];
+				arr[i+1] = temp;
+				// reset index to start from the beginning
 				i = 0;
 			}
+			i++;
 		}
 		return arr;
 	}
