49cdde9e1219179e6bb624ebdec80642c09ca272
Cybertek Studio <cybertekstudio@wsip-70-166-39-41.dc.dc.cox.net>
2019-05-14 22:03:47 -0400

    Constructor -- toString 2

---
+++ src/day44_2/MethodsPratice.java
@@ -0,0 +1,17 @@
+package day44_2;
+
+public class MethodsPratice {
+	
+	MethodsPratice() {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		//can we do String.trim() ??
+		//Answer: no.
+		//trim is an instance method, not static.
+		String name = "Yasin";
+
+	}
+}
