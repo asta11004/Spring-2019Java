00244970ef58c5de914876f2a1365084c110db5f
Cybertek Studio <cybertekstudio@wsip-70-166-39-41.dc.dc.cox.net>
2019-05-21 22:09:27 -0400

    class recap -- inheritance intro evening sesion

---
+++ src/day48_night_session/Person.java
@@ -0,0 +1,40 @@
+package day48_night_session;
+
+public class Person {
+	private String name;
+	private int age;
+	private float height;
+	private char gender;
+	static int counter;
+	private static String cititizenship;
+
+
+	public Person(String name, int age, float height, char gender) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.gender = gender;
+		// whenever, new object get's created - counter will be increased.
+		// since constructor calls automatically, at the time when new object is
+		// created.
+		counter++;
+		System.out.println("New person created. Total count of people: " + counter);
+	}
+
+	public static String getCititizenship() {
+		return cititizenship;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", height=" + height + ", gender=" + gender + "]";
+	}
+	
+
+	static {
+		//this is where value of cititizenship was updated.
+		System.out.println("Hello from static block.");
+		cititizenship = "US";
+	}
+}
