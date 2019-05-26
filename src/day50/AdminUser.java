ef73f76911447763ed538f51b8dbbcd2440987a4
Cybertek Studio <cybertekstudio@wsip-70-166-39-41.dc.dc.cox.net>
2019-05-26 13:24:52 -0400

    method overriding

--- src/day50/AdminUser.java
+++
@@ -1,5 +0,0 @@
-package day50;
-
-public class AdminUser {
-
-}
---
+++ src/day51/Horse.java
@@ -0,0 +1,11 @@
+package day51;
+
+public class Horse extends Animal{
+	
+	@Override
+	public void makeNoise() {
+		System.out.println("Nai Nai");
+		
+	}
+
+}
