4b307165eb88cff25506d57a98dee7572ff2c5d1
Cybertek Studio <cybertekstudio@wsip-70-166-39-41.dc.dc.cox.net>
2019-05-29 22:06:48 -0400

    field and method hiding -- final keyword night session

---
+++ src/day53_night_session/Animal.java
@@ -0,0 +1,19 @@
+package day53_night_session;
+
+public class Animal {
+	//from now one, this method hidden,
+	//because sub class defines exactly same method
+	public static void m1() {
+		System.out.println("Animal class m1");
+	}
+	
+	public static void m2() {
+		System.out.println("Animal class m2");
+	}
+	
+	public void run() {
+		System.out.println("Animal is runing");
+	}
+	
+	
+}
---
+++ src/day53_night_session/AnimalTestClass.java
@@ -0,0 +1,15 @@
+package day53_night_session;
+
+public class AnimalTestClass {
+	public static void main(String[] args) {
+		Dog dog1 = new Dog();
+		dog1.run();
+		//in the Dog class
+		//we have same static method
+		//that means m1() from Animal class
+		//is hidden
+		Dog.m1(); 
+		Dog.m2();
+		
+	}
+}
